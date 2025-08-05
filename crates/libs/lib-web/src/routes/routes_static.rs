e27e1fe101f6247238bfe51aa3765f30f0b10bb2
wangxuwei84 <wangxuwei84@gmail.com>
2025-08-05 14:23:45 +0800

    - fix resource not found when refresh page

--- crates/libs/lib-web/src/routes/routes_static.rs
+++ crates/libs/lib-web/src/routes/routes_static.rs
@@ -1,17 +1,31 @@
 use axum::handler::HandlerWithoutStateExt;
-use axum::http::StatusCode;
+use axum::http::{StatusCode, Uri};
+use axum::response::{Html, IntoResponse};
 use axum::routing::{any_service, MethodRouter};
+use std::ffi::OsStr;
+use std::fs::read_to_string;
+use std::path::Path;
 use tower_http::services::ServeDir;
 
 // Note: Here we can just return a MethodRouter rather than a full Router
 //       since ServeDir is a service.
 pub fn serve_dir(web_folder: &'static String) -> MethodRouter {
-	async fn handle_404() -> (StatusCode, &'static str) {
-		(StatusCode::NOT_FOUND, "Resource not found.")
-	}
+	let path = format!("{}{}", web_folder, "./index.html");
+	let handle_404 = async move |uri: Uri| {
+		match get_extension(uri.path()) {
+			Some(_) => {
+				(StatusCode::NOT_FOUND, "Resource not found.").into_response()
+			}
+			// for no extesion and render in client side
+			None => (StatusCode::OK, Html(read_to_string(&path).unwrap()))
+				.into_response(),
+		}
+	};
 
-	any_service(
-		ServeDir::new(web_folder)
-			.not_found_service(handle_404.into_service()),
-	)
+	// use .fallback instead of .not_found_service, this way, can handle status code
+	any_service(ServeDir::new(web_folder).fallback(handle_404.into_service()))
+}
+
+fn get_extension(file_name: &str) -> Option<&str> {
+	Path::new(file_name).extension().and_then(OsStr::to_str)
 }
